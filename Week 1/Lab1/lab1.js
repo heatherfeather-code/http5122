0439ca57afaab70864934a0fb7a48860ba977552
Heather Boden <heatherboden3@gmail.com>
2024-09-10 13:13:50 -0400

    Adding Week one labs to github

---
+++ Week 1/Lab1/lab1.js
@@ -0,0 +1,10 @@
+// alert("Hello World!");
+
+var name ="Heather"; //String Data type
+var age = 27; //number data type
+var daysInYear = 365; //number data type
+
+var multipleNumber = age * daysInYear;
+var results = " days old...more or less.";
+
+alert(name + " is " + multipleNumber+ results);
